38009aaf46439d038b25bf2c06bfa75af43d9fcf
kasaraplokesh17 <kasaraplokesh@gmail.com>
2026-06-05 09:08:13 +0530

    Delete ollive-inference-main directory

--- ollive-inference-main/backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from app.models.conversation import Conversation, Message, ConversationStatus, MessageRole
-from app.models.inference_log import InferenceLog, RequestStatus
-
-__all__ = [
-    "Conversation", "Message", "ConversationStatus", "MessageRole",
-    "InferenceLog", "RequestStatus",
-]
